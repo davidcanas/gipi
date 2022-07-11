f5ea40a8ce65f966ec8bc2cd0f2a4032bef5f79a
David <dg0837.dc@gmail.com>
2022-07-11 08:40:43 +0100

    v0.2.3

--- install.sh
+++ install.sh
@@ -3,9 +3,13 @@ if [ "$EUID" -ne 0 ]
   then echo "Please run as root"
   exit
 fi
+echo "1/4 downloading the dependencies (requests/tracecolor)"
 pip install -r requirements.txt
+echo "2/4 Copying gipi.py file to gipi file"
 cp gipi.py gipi
+echo "3/4 Making the file executable" 
 chmod +x gipi
+echo "4/4 Moving gipi file to /usr/bin"
 cp gipi /usr/bin/
 
 echo "GIPI installed with success! Use gipi help to see all commands!"
